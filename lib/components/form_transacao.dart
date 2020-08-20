88b5a7850df18f446a7d6e70abb20263bc3a7cbb
Julia de Almeida Lopes <gitjulia1@gmail.com>
2020-08-20 12:57:51 -0300

    alteração form de nova transação << add datePicker>>

--- lib/components/form_transacao.dart
+++ lib/components/form_transacao.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class FormTransacao extends StatefulWidget {
   final void Function(String, double) onSubmit;
@@ -10,18 +12,35 @@ class FormTransacao extends StatefulWidget {
 }
 
 class _FormTransacaoState extends State<FormTransacao> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _valorController = TextEditingController();
+  DateTime _dataselecionada;
 
-  final valorController = TextEditingController();
+  _submitForm() {
+    final title = _titleController.text;
+    final value = double.tryParse(_valorController.text) ?? 0.0;
 
-  _submitForm () {
-    final title = titleController.text;
-    final value = double.tryParse(valorController.text) ?? 0.0;
-
-    if(title.isEmpty || value <= 0){
+    if (title.isEmpty || value <= 0) {
       return;
     }
     widget.onSubmit(title, value);
+  }
+
+  _abrirDatePicker() {
+    showDatePicker(
+      cancelText: 'CANCELAR',
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _dataselecionada = pickedDate;
+      });
+    });
   }
 
   @override
@@ -33,20 +52,48 @@ class _FormTransacaoState extends State<FormTransacao> {
         child: Column(
           children: <Widget>[
             TextField(
-              controller: titleController,
+              controller: _titleController,
               onSubmitted: (_) => _submitForm(),
               decoration: InputDecoration(labelText: 'Título: '),
             ),
             TextField(
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               onSubmitted: (_) => _submitForm(),
-              controller: valorController,
+              controller: _valorController,
               decoration: InputDecoration(labelText: 'Valor (R\$): '),
+            ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _dataselecionada == null
+                          ? 'Nenhuma data selecionada'
+                          : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_dataselecionada)}',
+                      style: GoogleFonts.inter(
+                          textStyle: TextStyle(
+                              fontSize: 16, color: Color(0xFFff500f))),
+                    ),
+                  ),
+                  FlatButton(
+                    child: Text(
+                      'Selecionar data',
+                      style: GoogleFonts.inter(
+                          textStyle: TextStyle(
+                              color: Color(0xFF6c757d),
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    onPressed: _abrirDatePicker,
+                  )
+                ],
+              ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                FlatButton(
+                RaisedButton(
+                  color: Color(0xFFe1e2e6),
                   onPressed: _submitForm,
                   child: Text(
                     'Nova Transação',
